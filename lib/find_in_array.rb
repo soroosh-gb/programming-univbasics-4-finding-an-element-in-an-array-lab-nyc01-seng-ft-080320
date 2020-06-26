d4586b3180d66a24993a2a08f9f5abbeef0cd902
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-26 23:40:07 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,10 @@ def find_element_index(array, value_to_find)
   result = []
   counter = 0 
  while counter < array.length do
-   
+   if array[counter] == value_to_find {
+     result.push(array[counter])
+   } else
+   counter += 1 
  end
  end
 
